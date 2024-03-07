d908cfc7a6cdf7762906658e2ff79e83a422a862
Meoneff <hahoangduy95@gmail.com>
2024-03-07 21:29:29 +0700

    step 2

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,7 +4,14 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideRouter(routes), importProvidersFrom(TuiRootModule)]
+  providers: [provideAnimations(), provideRouter(routes), importProvidersFrom(TuiRootModule), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"motor-844f4","appId":"1:87271892749:web:195d52816d62f4efe8b244","storageBucket":"motor-844f4.appspot.com","apiKey":"AIzaSyBqzNB8SLlzsm6TLd8b6K2pFsWsOxm6A0U","authDomain":"motor-844f4.firebaseapp.com","messagingSenderId":"87271892749","measurementId":"G-90YM9HT281"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideMessaging(() => getMessaging())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
