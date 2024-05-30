3b4a05643f5d12d3c39411c549b4239cfd3dc656
Meoneff <hahoangduy95@gmail.com>
2024-05-30 11:39:41 +0700

    finish detail

--- client/src/app/pages/layout/motorbike/detail/detail.component.ts
+++ client/src/app/pages/layout/motorbike/detail/detail.component.ts
@@ -47,12 +47,6 @@ export class DetailComponent {
   city = ['Thành phố Hồ Chí Minh', 'Hà Nội', 'Huế'];
 
   chooseCity = new FormControl();
-
-  // motorcycleForm = new FormGroup({
-  //   numberOfMotorcycles: new FormControl(),
-  //   startDate: new FormControl(),
-  //   endDate: new FormControl(),
-  // });
   selectedMotor$: Observable<Motor> = this.store.select(
     'motor',
     'selectedMotor',
@@ -92,11 +86,16 @@ export class DetailComponent {
   isFirstZeroInEndDate: boolean = false;
   isUpdateStatusMotorOneTime = false;
   reservation_id: string = '';
-  selectedDays: number = 0;
+  selectedDays: number = 1;
   motor_id: string = '';
+  total: number = 0;
   subcriptions: Subscription[] = [];
 
+  @ViewChild('dateCheckin') dateCheckin!: ElementRef;
+  @ViewChild('dateCheckout') dateCheckout!: ElementRef;
+
   constructor(
+    private cd: ChangeDetectorRef,
     private motorService: MotorService,
     private router: Router,
     private route: ActivatedRoute,
@@ -133,6 +132,54 @@ export class DetailComponent {
     );
   }
 
+  // khai báo gần như ngOnInit
+  ngAfterViewInit(): void {
+    this.setupDatePickers();
+  }
+
+  //khai báo thêm để cho chúng sẵn sàng truy cập và thao tác bổ sung vào
+
+  setupDatePickers(): void {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    this.dateCheckin.nativeElement.valueAsDate = today;
+    this.dateCheckout.nativeElement.valueAsDate = tomorrow;
+
+    this.dateCheckin.nativeElement.min = today.toISOString().split('T')[0];
+    const minDateCheckout = new Date(today);
+    minDateCheckout.setDate(today.getDate() + 1);
+    this.dateCheckout.nativeElement.min = minDateCheckout
+      .toISOString()
+      .split('T')[0];
+
+    this.dateCheckin.nativeElement.addEventListener('input', () => {
+      this.updateTotalDays();
+      const checkinDate = new Date(this.dateCheckin.nativeElement.value);
+      const checkoutDate = new Date(this.dateCheckout.nativeElement.value);
+      if (checkinDate > checkoutDate) {
+        const newCheckoutDate = new Date(checkinDate);
+        newCheckoutDate.setDate(newCheckoutDate.getDate() + 1);
+        this.dateCheckout.nativeElement.valueAsDate = newCheckoutDate;
+      }
+      const timeDiff = Math.abs(checkoutDate.getTime() - checkinDate.getTime());
+      this.selectedDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    });
+
+    this.dateCheckout.nativeElement.addEventListener('input', () => {
+      this.updateTotalDays();
+      const checkinDate = new Date(this.dateCheckin.nativeElement.value);
+      const checkoutDate = new Date(this.dateCheckout.nativeElement.value);
+      if (checkoutDate < checkinDate) {
+        const newCheckinDate = new Date(checkoutDate);
+        newCheckinDate.setDate(newCheckinDate.getDate() - 1);
+        this.dateCheckin.nativeElement.valueAsDate = newCheckinDate;
+      }
+      const timeDiff = Math.abs(checkoutDate.getTime() - checkinDate.getTime());
+      this.selectedDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    });
+  }
+
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
@@ -140,6 +187,97 @@ export class DetailComponent {
         this.store.dispatch(MotorActions.getMotorById({ motorId: id }));
       }
     });
+
+    //date-pikcer
+    const dateContainers = document.querySelectorAll('.input-container');
+    dateContainers.forEach((dateContainer) => {
+      const dateInput = dateContainer.querySelector(
+        '.date-field',
+      ) as HTMLInputElement;
+      if (dateInput) {
+        dateContainer.addEventListener('click', (event) => {
+          dateInput.select();
+        });
+      }
+    });
+    const dateCheckin = document.getElementById(
+      'date-checkin',
+    ) as HTMLInputElement;
+    const dateCheckout = document.getElementById(
+      'date-checkout',
+    ) as HTMLInputElement;
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    dateCheckin.valueAsDate = today;
+    dateCheckout.valueAsDate = tomorrow;
+
+    // Không cho phép chọn ngày trước ngày hiện tại
+    dateCheckin.min = today.toISOString().split('T')[0];
+    const minDateCheckout = new Date(today);
+    minDateCheckout.setDate(today.getDate() + 1);
+    dateCheckout.min = minDateCheckout.toISOString().split('T')[0];
+
+    dateCheckin.addEventListener('input', () => {
+      this.updateTotalDays();
+      const checkinDate = new Date(dateCheckin.value);
+      const checkoutDate = new Date(dateCheckout.value);
+      if (checkinDate > checkoutDate) {
+        const newCheckoutDate = new Date(checkinDate);
+        newCheckoutDate.setDate(newCheckoutDate.getDate() + 1);
+        dateCheckout.valueAsDate = newCheckoutDate;
+      }
+      // Tính toán tổng số ngày
+      const timeDiff = Math.abs(checkoutDate.getTime() - checkinDate.getTime());
+      this.selectedDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      // this.totalCost = this.selectedDays * this.selectedMotor. + 7900 * 2;
+      // Hiển thị tổng tiền trên giao diện
+      // const totalCostElement = document.getElementById('total-cost');
+      // if (totalCostElement) {
+      //   totalCostElement.textContent = `${this.totalCost}`;
+      // }
+    });
+
+    dateCheckout.addEventListener('input', () => {
+      this.updateTotalDays();
+      const checkinDate = new Date(dateCheckin.value);
+      const checkoutDate = new Date(dateCheckout.value);
+      if (checkoutDate < checkinDate) {
+        const newCheckinDate = new Date(checkoutDate);
+        newCheckinDate.setDate(newCheckinDate.getDate() - 1);
+        dateCheckin.valueAsDate = newCheckinDate;
+      }
+      // Tính toán tổng số ngày
+      const timeDiff = Math.abs(checkoutDate.getTime() - checkinDate.getTime());
+      this.selectedDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      // this.totalCost = this.selectedDays * this.selectCar.price + 7900 * 2;
+      // Hiển thị tổng tiền trên giao diện
+      // const totalCostElement = document.getElementById('total-cost');
+      // if (totalCostElement) {
+      //   totalCostElement.textContent = `${this.totalCost}`;
+      // }
+    });
+    //date-pikcer
+  }
+
+  updateTotalDays() {
+    const dateCheckin = document.getElementById(
+      'date-checkin',
+    ) as HTMLInputElement;
+    const dateCheckout = document.getElementById(
+      'date-checkout',
+    ) as HTMLInputElement;
+
+    const checkinDate = new Date(dateCheckin.value);
+    const checkoutDate = new Date(dateCheckout.value);
+
+    if (checkinDate && checkoutDate) {
+      const timeDiff = checkoutDate.getTime() - checkinDate.getTime();
+      const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      this.selectedDays = daysDiff; // Cập nhật giá trị tổng ngày
+      // Trigger change detection
+      this.cd.detectChanges();
+    }
   }
   item = this.motorService.getMotorDetail();
 
@@ -153,6 +291,34 @@ export class DetailComponent {
       return parseInt(formControl.value);
     }
     return 0; // Trả về 0 nếu không tìm thấy form control hoặc giá trị không hợp lệ
+  }
+
+  totalCost(): number {
+    if (
+      !this.item ||
+      !this.item.price ||
+      !this.selectedDays ||
+      !this.updateQuantity()
+    ) {
+      // Trả về 0 nếu không có chi tiết hoặc thông tin giá hoặc số ngày thuê hoặc số lượng chưa được chọn
+      return 0;
+    }
+
+    const rentalPrice = this.item.price;
+    const quantity = this.updateQuantity();
+    const rentalDays = this.selectedDays;
+
+    // Tính toán tổng giá trị thuê xe
+    const rentalTotal = rentalPrice * quantity * rentalDays;
+
+    // Các khoản phí cố định
+    const serviceFee = 5;
+    const insuranceFee = 5;
+
+    // Tổng giá trị
+    const total = rentalTotal + serviceFee + insuranceFee;
+
+    return total;
   }
 
   goToExtras() {
